06964364cb7e81e1f8fb67d45ac3dbad91169052
Cerberus <shadow_sun0708@hotmail.com>
2018-12-24 17:43:11 +0700

    clean code

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,11 +1,3 @@
-import { TEST } from 'test'
-
-const f = <T>(value: T): T => value
-
 describe('general', () => {
-	it('simple', async () => {
-		const test = f(1)
-		TEST // ?
-		test // ?
-	})
+	it('simple', async () => {})
 })
