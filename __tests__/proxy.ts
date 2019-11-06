5b452eb3356adea8d6245b19f03634eb37ea9cad
Cerberus <shadow_sun0708@hotmail.com>
2019-11-06 10:42:29 +0700

    try nested proxy

--- __tests__/proxy.ts
+++ __tests__/proxy.ts
@@ -1,23 +1,53 @@
-type Proxify<T> = { [P in keyof T]: T[P] }
+describe('proxy', () => {
+	it('nested proxy', () => {
+		type Proxify<T> = { [P in keyof T]: T[P] }
 
-const handler = {
-	get(obj: any, key: any) {
-		return obj[key]
-	},
-}
+		const handler = {
+			get(obj: any, key: any) {
+				return obj[key]
+			},
+		}
 
-function proxify<T>(o: T): Proxify<T> {
-	return new Proxy(o, handler)
-}
+		function proxify<T>(o: T): Proxify<T> {
+			return new Proxy(o, handler)
+		}
 
-const props = {
-	a: (test: string) => {},
-}
+		const props = {
+			a: (test: string) => {},
+		}
 
-const proxyProps = proxify(props)
+		const proxyProps = proxify(props)
 
-proxyProps.a
+		proxyProps.a
+	})
+	it('nested add', () => {
+		const req = {
+			a: {
+				b: (grpcMeta?: number) => {
+					grpcMeta /*?*/
+					return 1 + (grpcMeta as number)
+				},
+			},
+			grpcMeta: 0,
+		}
+		const proxy = new Proxy<typeof req>(req, {
+			get(req: any, key: any) {
+				req[key].grpcMeta = req.grpcMeta
 
-describe('none', () => {
-	it('none', () => {})
+				return new Proxy<any>(req[key], {
+					get(req: any, key: any) {
+						req[key].grpcMeta = req.grpcMeta
+
+						return new Proxy<any>(req[key], {
+							apply(target, thisArg, args) {
+								return target.apply(thisArg, [...args, target.grpcMeta])
+							},
+						})
+					},
+				})
+			},
+		})
+		proxy.grpcMeta = 1
+		proxy.a.b() /*?*/
+	})
 })
---
+++ src/file.ts
@@ -0,0 +1,3 @@
+export const subTest = () => {}
+
+export default () => {}
