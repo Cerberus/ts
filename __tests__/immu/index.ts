55e21355e6534e8d9f2c71076385b2d20433eb0b
Cerberus <shadow_sun0708@hotmail.com>
2020-01-02 17:22:37 +0700

    add constrcutor

--- __tests__/immu/index.ts
+++ __tests__/immu/index.ts
@@ -1,17 +1,32 @@
-import 'prototype'
+import { Map } from 'immu'
+require('prototype')
 
-describe.only('immut', () => {
-	it('get', () => {
-		const obj = { a: 'value', b: {} }
-		expect(obj.get('a')).toEqual('value')
-		expect(obj.get('b')).toEqual({})
-		expect(obj.get('b') === obj.get('b')).toBeTruthy()
+describe('immu', () => {
+	describe('Map', () => {
+		it('constrcutor', () => {
+			const obj = {}
+			expect(Map()).toBe(Map())
+			expect(Map(obj)).toBe(Map(obj))
+			expect(Map(obj)).toBe(obj)
+		})
+		it('get', () => {
+			const obj = { a: 'value', b: {} }
+			expect(obj.get('a')).toEqual('value')
+			expect(obj.get('b')).toEqual({})
+			expect(obj.get('b') === obj.get('b')).toBeTruthy()
+		})
+		it('getIn', () => {
+			const obj = { a: { b: 'value' } }
+			expect(obj.getIn(['a', 'b'])).toEqual('value')
+			expect(obj.getIn(['a', 'c'])).toEqual(undefined)
+			expect(obj.getIn(['a', 'b', 'c'])).toEqual(undefined)
+			expect(obj.getIn(['a']) === obj.getIn(['a'])).toBeTruthy()
+		})
 	})
-	it('getIn', () => {
-		const obj = { a: { b: 'value' } }
-		expect(obj.getIn(['a', 'b'])).toEqual('value')
-		expect(obj.getIn(['a', 'c'])).toEqual(undefined)
-		expect(obj.getIn(['a', 'b', 'c'])).toEqual(undefined)
-		expect(obj.getIn(['a']) === obj.getIn(['a'])).toBeTruthy()
+	describe('List', () => {
+		it('size', () => {
+			// single of truth of empty map
+			// proxy size to length instead
+		})
 	})
 })
---
+++ src/immu/index.ts
@@ -0,0 +1,3 @@
+export const EMPTY_OBJECT = {}
+// tslint:disable-next-line
+export const Map = <T>(obj?: T) => obj || EMPTY_OBJECT
