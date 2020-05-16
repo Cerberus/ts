79bb713847915b63d3e2f5bcbf5c4dcf9d99156d
Cerberus <shadow_sun0708@hotmail.com>
2020-05-16 19:19:01 +0700

    fix jest. support optional chaining

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,6 +1,3 @@
 describe.only('index', () => {
-	it('test', async () => {
-		const host = 'api.starter-app.xyz'
-		const origin = 'https://starter-app.xyz'
-	})
+	it('test', async () => {})
 })
--- __tests__/lodash.ts
+++ __tests__/lodash.ts
@@ -1,3 +1,5 @@
+import { isEqual } from 'lodash'
+
 describe('lodash', () => {
 	it('simple', () => {})
 })
