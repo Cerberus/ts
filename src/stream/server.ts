a933c8eab820ac9ff1035213a73a366886d7c51f
Cerberus <shadow_sun0708@hotmail.com>
2020-08-25 17:07:29 +0700

    commit

--- src/stream/server.ts
+++ src/stream/server.ts
@@ -4,7 +4,6 @@ import * as http from 'http'
 const server = http.createServer()
 
 server.on('request', (req, res) => {
-	// tslint:disable-next-line: prefer-template
 	const fileStream = fs.createReadStream(__dirname + '/file.txt')
 	fileStream.pipe(res)
 })
