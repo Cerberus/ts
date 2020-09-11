4e7270984c67f327fe752a2a4feb88d2961160ad
Cerberus <shadow_sun0708@hotmail.com>
2020-09-11 16:29:21 +0700

    remove mocking

--- __tests__/script.ts
+++ __tests__/script.ts
@@ -1,8 +1,7 @@
-import { add, devide } from '@cerberus/script';
+import { add } from '@cerberus/script';
 
 describe('script', () => {
 	it.only('simple', () => {
 		add(1, 2); /*?*/
-		devide(1, 2);
 	});
 });
